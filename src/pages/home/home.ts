11361e147c00ed2423f254d8988bacb91f9b2383
EFabrizio <fabriziocastellanos@hotmail.com>
2018-04-08 22:21:00 -0600

    Add update function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,4 +11,58 @@ export class HomePage {
 
   }
 
+update(todo, i) {
+    let alert = this.alertCtrl.create({
+      title: 'Modificar Tarea',
+      message: '¿Qué deseas hacer?',
+      inputs: [
+        {
+          name: 'nombre',
+          placeholder: 'Nombre',
+          value: todo.nombre
+        },
+        {
+          name: 'fecha',
+          type: 'date',
+          value: todo.fecha
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },
+        {
+          text: 'Aceptar',
+          handler: (data: any) => {
+            this.todos[i] = { nombre: data.nombre, fecha: data.fecha, hecho: todo.hecho };
+            this.save(todo);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  delete(todo, i) {
+    let alert = this.alertCtrl.create({
+      title: 'Eliminar Tarea',
+      message: ¿Seguro que deseas eliminar ${todo.nombre}?,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },
+        {
+          text: 'Aceptar',
+          handler: () => {
+            this.todos.splice(i, 1);
+            this.save(todo);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
 }
